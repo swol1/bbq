0a37892f279f85d7280e83dab2b309d69e167c53
swol1 <den.gavrilin1@gmail.com>
2018-12-23 00:16:26 +0300

    Added login, signup forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def user_avatar(user)
     asset_path('user.png')
   end
+
+  def form_errors_for(object=nil)
+    render('shared/form_errors', object: object) unless object.blank?
+  end
 end
