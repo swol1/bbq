8ec720edf358b89050e20d0cfbbfc977fa32f61c
swol1 <den.gavrilin1@gmail.com>
2018-12-26 01:52:01 +0300

    event creator no subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,10 +7,12 @@ class SubscriptionsController < ApplicationController
     @new_subscription = @event.subscriptions.build(subscription_params)
     @new_subscription.user = current_user
 
-    if @new_subscription.save
-      redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
-    else
-      render 'events/show', alert: I18n.t('controllers.subscriptions.error')
+    unless current_user.id == @event.user_id
+      if @new_subscription.save
+        redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
+      else
+        render 'events/show', alert: I18n.t('controllers.subscriptions.error')
+      end
     end
   end
 
