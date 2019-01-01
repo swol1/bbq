63dd41bcd592beecea762bb04b8b8f3a454d8a92
swol1 <den.gavrilin1@gmail.com>
2019-01-01 17:43:01 +0300

    fog-aws for heroku

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module Bbq
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -32,5 +30,6 @@ module Bbq
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.i18n.default_locale = :ru
+    config.assets.initialize_on_precompile = false
   end
 end
