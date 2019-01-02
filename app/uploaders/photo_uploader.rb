f184ee21e81c929b7d7b13e505a9514183b8f589
swol1 <den.gavrilin1@gmail.com>
2019-01-02 23:43:08 +0300

    added s3 region

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,7 +3,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include CarrierWave::RMagick
 
-
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
     storage :fog
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
+      region:                ENV['S3_BUCKET_REGION'],
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
