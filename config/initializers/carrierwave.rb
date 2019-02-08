234b5502e1d850322029406ebb4ac4defb37bbe2
swol1 <den.gavrilin1@gmail.com>
2019-02-09 02:34:19 +0300

    vps deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,11 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      region:                Rails.application.credentials.aws[:bucket_region!],
-      aws_access_key_id:     Rails.application.credentials.aws[:access_key_id!],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key!]
+      region:                Rails.application.credentials.aws[:bucket_region],
+      aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
     }
 
-    config.fog_directory  = Rails.application.credentials.aws[:bucket_name!]
+    config.fog_directory  = Rails.application.credentials.aws[:bucket_name]
   end
 end
