6af16052c139b5a64ad45a73ba2c32fe6e7e37d8
swol1 <den.gavrilin1@gmail.com>
2019-02-09 12:43:30 +0300

    mailgun configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  root "events#index"
 
   resources :events do
     resources :comments, only: [:create, :destroy]
