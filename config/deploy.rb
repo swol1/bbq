415c3448bd77e09a1bd04bd834516cba96d4a385
swol1 <den.gavrilin1@gmail.com>
2019-02-09 12:06:14 +0300

    vps deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :branch, ENV['BRANCH'] if ENV['BRANCH']
 append :linked_files, "config/master.key", "config/database.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
