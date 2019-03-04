0b9136f67f1660cbaf34c6dd2c7a30df0976467d
swol1 <den.gavrilin1@gmail.com>
2019-03-04 22:15:20 +0300

    Updated helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
   def has_this_event_subscription?
-    current_user.subscriptions.ids.any? { |sub| @event.subscriptions.ids.include?(sub) }
+    (current_user.subscriptions.ids & @event.subscriptions.ids).any?
   end
 end
